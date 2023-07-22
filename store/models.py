970eb8462c2da7ae73da3a639781ee63db65a28c
gleysonwener <gleysonwener3@gmail.com>
2023-07-21 22:01:05 -0300

    cart and checkout

--- store/models.py
+++ store/models.py
@@ -14,9 +14,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
@@ -28,12 +37,29 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all() # buscando os itens do pedido
+        total = sum([item.get_total for item in orderitems]) # percorre todos os itens do pedido e realiza a soma
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all() # buscando os itens do pedido
+        total = sum([item.quantity for item in orderitems]) # percorre todas as quantidades de  itens do pedido e realiza a soma
+        return total
+    
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
 
 class shippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
--- store/views.py
+++ store/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store.html', context)
 
 
 def cart(request):
-    context = {}
+    # se o usuário estiver autenticado
+    if request.user.is_authenticated:
+        customer = request.user.customer # recebe o campo customer de user
+        order, created = Order.objects.get_or_create(customer=customer, complete=False) # crinado ou obtendo um pedido
+        items = order.orderitem_set.all() # recebendo todos os item do pedido
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'cart.html', context )
 
 
 def checkout(request):
-    context = {}
+     # se o usuário estiver autenticado
+    if request.user.is_authenticated:
+        customer = request.user.customer # recebe o campo customer de user
+        order, created = Order.objects.get_or_create(customer=customer, complete=False) # crinado ou obtendo um pedido
+        items = order.orderitem_set.all() # recebendo todos os item do pedido
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'checkout.html', context )
 
 
